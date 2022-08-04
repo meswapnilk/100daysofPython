31ccd472d76c98ebd0baf63fcca55da663b2767b
Swapnil Kulkarni <meswapnilkulkarni@gmail.com>
2022-08-04 11:13:03 +0530

    Day 8 Program 1

---
+++ Day8/001-main.py
@@ -0,0 +1,19 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+def greet():
+    print("Hello")
+    print("How do you do?")
+    print("Isn't the weather nice today?")
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}? ")
+    print("Isn't the weather nice today?")
+
+greet()
+greet_with_name("Swapnil")
+

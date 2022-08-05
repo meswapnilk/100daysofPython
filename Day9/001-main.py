85a4314c688fa876622984ac20382abfe0b9008f
Swapnil Kulkarni <meswapnilkulkarni@gmail.com>
2022-08-05 18:41:51 +0530

    Updates to program 1

--- Day9/001-main.py
+++ Day9/001-main.py
@@ -1,3 +1,12 @@
 programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
 
 print(programming_dictionary["Bug"])
+
+empty_dictionary = {}
+
+empty_dictionary["bug"] = "nobug"
+
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])

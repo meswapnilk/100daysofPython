d2bfa1a8f0e96c91fb69d508652b8d12faf111d1
Swapnil Kulkarni <meswapnilkulkarni@gmail.com>
2022-08-08 19:32:58 +0530

    Day 13 Problem 6 Solution

--- Day13/006-main.py
+++ Day13/006-main.py
@@ -4,3 +4,10 @@
 #   print("You are a millenial.")
 # elif year > 1994:
 #   print("You are a Gen Z.")
+
+# Solution
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year >= 1994:
+  print("You are a Gen Z.")

c091d6aa38c1b4bcba63d763c3a347ee75c0353f
Swapnil Kulkarni <meswapnilkulkarni@gmail.com>
2022-08-09 20:27:33 +0530

    Day 17 Program 1 Update

--- Day17/001-main.py
+++ Day17/001-main.py
@@ -1,10 +1,24 @@
 class User:
+    # Constructor
     def __init__(self, user_id, user_name):
         self.id = user_id
         self.username = user_name
-        self.follower = 0
+        self.followers = 0
+        self.following = 0
+    
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 user1 = User(1, "SK")
+user2 = User(2, "AK")
 
-print(user1.id)
-print(user1.follower)
+user1.follow(user2)
+
+print("User 1 Details")
+print(user1.followers)
+print(user1.following)
+
+print("User 2 Details")
+print(user2.followers)
+print(user2.following)
